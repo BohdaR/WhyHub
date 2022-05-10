7d00ddc26854d82279e6814b1fe497c23d3c5657
BohdaR <bohdanshushval@gmail.com>
2022-05-10 20:41:10 +0300

    connected MySQL

--- WhyHub/settings.py
+++ WhyHub/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'WhyHub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
     }
 }
 
