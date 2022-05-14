14f357e09ff9ff29924cf859f30c7ba2934d9e3f
bogdan <bogdanshushval25@gmail.com>
2022-05-14 16:43:51 +0300

    changed prod settings

--- WhyHub/wsgi.py
+++ WhyHub/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WhyHub.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WhyHub.settings.prod')
 
 application = get_wsgi_application()
