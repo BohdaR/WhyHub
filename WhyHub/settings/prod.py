1283ab361508d36b02cf33b031c179eb7ab39299
bogdan <bogdanshushval25@gmail.com>
2022-05-14 21:55:30 +0300

    added CSRF_TRUSTED_ORIGINS host

--- WhyHub/settings/prod.py
+++ WhyHub/settings/prod.py
@@ -2,6 +2,8 @@ from .defaults import *
 
 DEBUG = False
 
+CSRF_TRUSTED_ORIGINS = ['http://34.118.100.219', 'https://127.0.0.1']
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
