31ec775f13ea1819837603f973a9880c18b88419
vadyk <kzus47@gmail.com>
2022-05-14 15:57:13 +0300

    burger2

--- shop/static/shop/js/jquery.js
+++ shop/static/shop/js/jquery.js
@@ -30,3 +30,55 @@ jQuery('<div class="quantity-nav"><button class="quantity-button quantity-up" ty
       });
 
     });
+    //Меню бургер
+    const isMobile = {
+      Android: function () {
+        return navigator.userAgent.match(/Android/i);
+      },
+      BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+      },
+      IOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+      },
+      Opera: function () {
+        return navigator.userAgent.match(/Opera/i);
+      },
+      Windows: function () {
+        return navigator.userAgent.match(/Windows/i);
+      },
+      any: function () {
+        return (
+          isMobile.Android() | isMobile.BlackBerry() ||
+          isMobile.IOS() ||
+          isMobile.Opera() ||
+          isMobile.Windows()
+        );
+      },
+    };
+    
+    if (isMobile.any()) {
+      document.body.classList.add("_pc");
+    } else {
+      document.body.classList.add("_touch");
+    
+      let menuArrows = document.querySelectorAll(".menu_arrow");
+      if (menuArrows.length > 0) {
+        for (let index = 0; index < menuArrows.length; index++) {
+          const menuArrow = menuArrows[index];
+          menuArrow.addEventListener("click", function (e) {
+            menuArrow.parentElement.classList.toggle("_active");
+          });
+        }
+      }
+    }
+
+const menuBody = document.querySelector(".menu_body");
+const iconMenu = document.querySelector(".menu_icon");
+if (iconMenu) {
+  iconMenu.addEventListener("click", function (e) {
+    iconMenu.classList.toggle("_active");
+    document.body.classList.toggle("_lock");
+    menuBody.classList.toggle("_active");
+  });
+}
