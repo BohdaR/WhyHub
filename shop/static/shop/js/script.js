0805ec4a13bc7ce3c8e55b192485090a1bdef777
bohdan <bogdanshushval25@gmail.com>
2022-05-17 20:57:24 +0300

    fix bags

--- shop/static/shop/js/script.js
+++ shop/static/shop/js/script.js
@@ -3,9 +3,10 @@ let Left = document.getElementsByClassName('Left');
 let Right = document.getElementsByClassName('Right');
 let i = 0
 
-
-console.log(0)
-slides[0].style.opacity = 1;
+if (slides.length>0) {
+  console.log(0)
+  slides[0].style.opacity = 1;
+}
 
 
 function left_next_slide() {
@@ -27,51 +28,54 @@ function right_next_slide() {
   slides[i].style.opacity = 1;
 }
 
-Left[0].addEventListener("click", left_next_slide);
-Right[0].addEventListener("click", right_next_slide);
+if (Left[0])
+  Left[0].addEventListener("click", left_next_slide);
 
-    //Меню бургер
-    const isMobile = {
-      Android: function () {
-        return navigator.userAgent.match(/Android/i);
-      },
-      BlackBerry: function () {
-        return navigator.userAgent.match(/BlackBerry/i);
-      },
-      IOS: function () {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-      },
-      Opera: function () {
-        return navigator.userAgent.match(/Opera/i);
-      },
-      Windows: function () {
-        return navigator.userAgent.match(/Windows/i);
-      },
-      any: function () {
-        return (
-          isMobile.Android() | isMobile.BlackBerry() ||
-          isMobile.IOS() ||
-          isMobile.Opera() ||
-          isMobile.Windows()
-        );
-      },
-    };
+if (Right[0])
+  Right[0].addEventListener("click", right_next_slide);
 
-    if (isMobile.any()) {
-      document.body.classList.add("_pc");
-    } else {
-      document.body.classList.add("_touch");
+//Меню бургер
+const isMobile = {
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  IOS: function () {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function () {
+    return navigator.userAgent.match(/Opera/i);
+  },
+  Windows: function () {
+    return navigator.userAgent.match(/Windows/i);
+  },
+  any: function () {
+    return (
+        isMobile.Android() | isMobile.BlackBerry() ||
+        isMobile.IOS() ||
+        isMobile.Opera() ||
+        isMobile.Windows()
+    );
+  },
+};
 
-      let menuArrows = document.querySelectorAll(".menu_arrow");
-      if (menuArrows.length > 0) {
-        for (let index = 0; index < menuArrows.length; index++) {
-          const menuArrow = menuArrows[index];
-          menuArrow.addEventListener("click", function (e) {
-            menuArrow.parentElement.classList.toggle("_active");
-          });
-        }
-      }
+if (isMobile.any()) {
+  document.body.classList.add("_pc");
+} else {
+  document.body.classList.add("_touch");
+
+  let menuArrows = document.querySelectorAll(".menu_arrow");
+  if (menuArrows.length > 0) {
+    for (let index = 0; index < menuArrows.length; index++) {
+      const menuArrow = menuArrows[index];
+      menuArrow.addEventListener("click", function (e) {
+        menuArrow.parentElement.classList.toggle("_active");
+      });
     }
+  }
+}
 
 const menuBody = document.querySelector(".menu_body");
 const iconMenu = document.querySelector(".menu_icon");
