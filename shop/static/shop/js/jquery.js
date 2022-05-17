e6ea954e4dc556c412fe4a0603f4a1a1dc874c05
bohdan <bogdanshushval25@gmail.com>
2022-05-17 18:48:12 +0300

    added async post request from cart

--- shop/static/shop/js/jquery.js
+++ shop/static/shop/js/jquery.js
@@ -1,84 +1,48 @@
 jQuery('<div class="quantity-nav"><button class="quantity-button quantity-up" type="submit">+</button><button class="quantity-button quantity-down" type="submit">-</button></div>').insertAfter('.quantity input');
-    jQuery('.quantity').each(function() {
-      var spinner = jQuery(this),
+jQuery('.quantity').each(function() {
+    var spinner = jQuery(this),
         input = spinner.find('input[type="number"]'),
         btnUp = spinner.find('.quantity-up'),
         btnDown = spinner.find('.quantity-down'),
         min = input.attr('min'),
         max = input.attr('max');
 
-      btnUp.click(function() {
+    btnUp.click(function() {
         var oldValue = parseFloat(input.val());
         if (oldValue >= max) {
-          var newVal = oldValue;
+            var newVal = oldValue;
         } else {
-          var newVal = oldValue + 1;
+            var newVal = oldValue + 1;
         }
         spinner.find("input").val(newVal);
         spinner.find("input").trigger("change");
-      });
+    });
 
-      btnDown.click(function() {
+    btnDown.click(function() {
         var oldValue = parseFloat(input.val());
         if (oldValue <= min) {
-          var newVal = oldValue;
+            var newVal = oldValue;
         } else {
-          var newVal = oldValue - 1;
+            var newVal = oldValue - 1;
         }
         spinner.find("input").val(newVal);
         spinner.find("input").trigger("change");
-      });
+    });
+
+});
+
+
+$(document).on('click', '#cart_post_button',function(e){
+    $.ajax({
+        type:'POST',
+        url:'/cart/add/'+$('input[name=product_id]').val()+'/',
+        data:{
+            update:$('input[name=update]').val(),
+            quantity:1,
+            csrfmiddlewaretoken:$('input[name=csrfmiddlewaretoken]').val(),
+            action: 'post'
+        },
 
     });
-    //Меню бургер
-    const isMobile = {
-      Android: function () {
-        return navigator.userAgent.match(/Android/i);
-      },
-      BlackBerry: function () {
-        return navigator.userAgent.match(/BlackBerry/i);
-      },
-      IOS: function () {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-      },
-      Opera: function () {
-        return navigator.userAgent.match(/Opera/i);
-      },
-      Windows: function () {
-        return navigator.userAgent.match(/Windows/i);
-      },
-      any: function () {
-        return (
-          isMobile.Android() | isMobile.BlackBerry() ||
-          isMobile.IOS() ||
-          isMobile.Opera() ||
-          isMobile.Windows()
-        );
-      },
-    };
-    
-    if (isMobile.any()) {
-      document.body.classList.add("_pc");
-    } else {
-      document.body.classList.add("_touch");
-    
-      let menuArrows = document.querySelectorAll(".menu_arrow");
-      if (menuArrows.length > 0) {
-        for (let index = 0; index < menuArrows.length; index++) {
-          const menuArrow = menuArrows[index];
-          menuArrow.addEventListener("click", function (e) {
-            menuArrow.parentElement.classList.toggle("_active");
-          });
-        }
-      }
-    }
-
-const menuBody = document.querySelector(".menu_body");
-const iconMenu = document.querySelector(".menu_icon");
-if (iconMenu) {
-  iconMenu.addEventListener("click", function (e) {
-    iconMenu.classList.toggle("_active");
-    document.body.classList.toggle("_lock");
-    menuBody.classList.toggle("_active");
-  });
-}
+    return false;
+});
