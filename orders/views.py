8a82dd047453bf0b20a06795f47d4afc5a180771
bogdan <bogdanshushval25@gmail.com>
2022-06-25 10:58:11 +0300

    changed order algorithm

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
+
+from shop.models import Product
 from .models import OrderItem
 from .forms import OrderCreateForm
 from django.views.generic import FormView
 from cart.cart import Cart
-from django.urls import reverse_lazy
 
 
 class OrderCreate(FormView):
@@ -12,12 +13,28 @@ class OrderCreate(FormView):
 
     def form_valid(self, form):
         cart = Cart(self.request)
+        errors = 'Недостатньо на складі: '
+        for item in cart:
+            if item['quantity'] > item['product'].stock:
+                errors += f"{item['product']} залишилось - {item['product'].stock}\n"
+
+        if errors != 'Недостатньо на складі: ':
+            return self.render_to_response(self.get_context_data(form=form, errors=errors))
+
         order = form.save()
         for item in cart:
             OrderItem.objects.create(order=order,
                                      product=item['product'],
                                      price=item['price'],
                                      quantity=item['quantity'])
+
+            product = Product.objects.get(id=item['product'].id)
+            product.stock = item['product'].stock - item['quantity']
+
+            if item['product'].stock - item['quantity'] == 0:
+                product.available = False
+            product.save()
+
         cart.clear()
         return render(self.request, 'orders/created.html',
                       {'order': order})
@@ -25,23 +42,3 @@ class OrderCreate(FormView):
     def form_invalid(self, form):
         """If the form is invalid, render the invalid form."""
         return self.render_to_response(self.get_context_data(form=form))
-
-# def order_create(request):
-#     cart = Cart(request)
-#     if request.method == 'POST':
-#         form = OrderCreateForm(request.POST)
-#         if form.is_valid():
-#             order = form.save()
-#             for item in cart:
-#                 OrderItem.objects.create(order=order,
-#                                          product=item['product'],
-#                                          price=item['price'],
-#                                          quantity=item['quantity'])
-#             # очистка корзины
-#             cart.clear()
-#             return render(request, 'orders/created.html',
-#                           {'order': order})
-#     else:
-#         form = OrderCreateForm
-#     return render(request, 'orders/create.html',
-#                   {'cart': cart, 'form': form})
